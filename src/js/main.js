c3d926c78187363d7bbc015b071154f20280861a
Nicolas Ojeda <nojedac_24@hotmail.com>
2024-02-29 23:54:43 -0500

    modificando

--- src/js/main.js
+++ src/js/main.js
@@ -1,2 +1,17 @@
-import 'bootstrap';
-import 'bootstrap/dist/css/bootstrap.min.css';
+
+document.addEventListener('DOMContentLoaded', function () {
+    const themeToggle = document.getElementById('bd-theme');
+    const body = document.body;
+
+    themeToggle.addEventListener('click', function () {
+        body.classList.toggle('dark-theme');
+        const isDarkMode = body.classList.contains('dark-theme');
+        localStorage.setItem('dark-mode', isDarkMode);
+    });
+
+    // Verificar el modo preferido del usuario almacenado en localStorage
+    const preferredMode = localStorage.getItem('dark-mode');
+    if (preferredMode === 'true') {
+        body.classList.add('dark-theme');
+    }
+});
